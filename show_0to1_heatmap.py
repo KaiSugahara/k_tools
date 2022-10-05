ec9696118eb4f41a002c258046327370ad292203
Kai Sugahara <git@ksugahara.com>
2022-10-05 19:17:10 +0900

    Add: arg. of dimension labels

--- show_0to1_heatmap.py
+++ show_0to1_heatmap.py
@@ -1,6 +1,6 @@
 import plotly.express as px
 
-def show_0to1_heatmap(arr, title=None, label_x=None, label_y=None, label_color=None):
+def show_0to1_heatmap(arr, title=None, label_x=None, label_y=None, label_color=None, x=None, y=None):
 
-    fig = px.imshow(arr, text_auto='.2f', aspect="auto", color_continuous_scale='gray_r', title=title, labels=dict(x=label_x, y=label_y, color=label_color))
+    fig = px.imshow(arr, text_auto='.2f', aspect="auto", color_continuous_scale='gray_r', title=title, labels=dict(x=label_x, y=label_y, color=label_color), x=x, y=y)
     return fig
