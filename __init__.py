c7fe8e545a754b2221de59f0e6178151383241be
Kai Sugahara <kai@fulfills.jp>
2022-10-14 04:48:36 +0000

    Add: new func

--- __init__.py
+++ __init__.py
@@ -1,2 +1,3 @@
 from .add_array2csv import *
 from .show_0to1_heatmap import *
+from .print_head import *
---
+++ print_head.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from IPython.display import display
+from pprint import pprint
+
+def print_head(data, rows=5):
+    
+    # DataFrame
+    if type(data) in [pd.core.frame.DataFrame]:
+        
+        pd.set_option("display.max_rows", rows)
+        display(data)
+        pd.reset_option("display.max_rows")
+        
+    # Dict
+    if type(data) is dict:
+        
+        pprint( dict(zip(list(data.keys())[:rows], list(data.values())[:rows]))  )
+        print("length:", len(data))
+        
+    # List
+    if type(data) is list:
+        
+        pprint( data[:rows] )
+        print("length:", len(data))
