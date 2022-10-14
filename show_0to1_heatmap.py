1f3f28b5ce8ede30877ea9f79db307202df8ce60
Kai Sugahara <kai@fulfills.jp>
2022-10-14 04:48:12 +0000

    Update: set zmax and zmin

--- show_0to1_heatmap.py
+++ show_0to1_heatmap.py
@@ -2,5 +2,5 @@ import plotly.express as px
 
 def show_0to1_heatmap(arr, title=None, label_x=None, label_y=None, label_color=None, x=None, y=None):
 
-    fig = px.imshow(arr, text_auto='.2f', aspect="auto", color_continuous_scale='gray_r', title=title, labels=dict(x=label_x, y=label_y, color=label_color), x=x, y=y)
+    fig = px.imshow(arr, text_auto='.3f', aspect="auto", color_continuous_scale='gray_r', title=title, labels=dict(x=label_x, y=label_y, color=label_color), x=x, y=y, zmax=1, zmin=0)
     return fig
