ac9df797cb77ab0ea4c7a14c0251f1d2f73e1b24
Kai Sugahara <git@ksugahara.com>
2022-10-21 07:06:28 +0000

    Update: 結果をリストで返す

--- map_with_mp_and_tqdm.py
+++ map_with_mp_and_tqdm.py
@@ -3,9 +3,12 @@ from multiprocessing import Pool
 
 def map_with_mp_and_tqdm(func, data, processes=8):
 
+    results = []
     pool = Pool(processes=processes)
 
     with tqdm(total=len(data)) as t:
-
-        for _ in pool.imap_unordered(func, data):
+        for result in pool.imap(func, data):
+            results.append(result)
             t.update(1)
+
+    return results
