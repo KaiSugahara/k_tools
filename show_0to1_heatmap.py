55a185c5ecfadd3fd64caa109bff4e0e8a31e2b7
Kai Sugahara <git@ksugahara.com>
2023-05-16 16:07:48 +0900

    Update:

--- show_0to1_heatmap.py
+++ show_0to1_heatmap.py
@@ -1,6 +1,6 @@
-import plotly.express as px
-
 def show_0to1_heatmap(arr, title=None, label_x=None, label_y=None, label_color=None, x=None, y=None):
+
+    import plotly.express as px
 
     fig = px.imshow(arr, text_auto='.3f', aspect="auto", color_continuous_scale='gray_r', title=title, labels=dict(x=label_x, y=label_y, color=label_color), x=x, y=y, zmax=1, zmin=0)
     return fig
